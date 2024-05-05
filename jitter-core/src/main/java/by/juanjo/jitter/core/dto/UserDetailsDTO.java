31eba3a93dae0ed65f861adbbdf8f6de15b445d7
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-05 15:52:05 +0200

    Added forgotten DTO

--- jitter-core/src/main/java/by/juanjo/jitter/core/dto/UserDetailsDTO.java
+++ jitter-core/src/main/java/by/juanjo/jitter/core/dto/UserDetailsDTO.java
@@ -1,13 +1,12 @@
 package by.juanjo.jitter.core.dto;
 
-import by.juanjo.jitter.core.entity.UserPreference;
 import java.io.Serializable;
 import java.util.Set;
 import lombok.Data;
 
 public @Data class UserDetailsDTO implements Serializable {
 
-  private UserPreference userPreferences;
+  private UserPreferenceDTO userPreferences;
   private Set<CommentDTO> userComments;
   private Set<PostSummaryDTO> userPosts;
   private Set<UserFollowerDTO> followers;
