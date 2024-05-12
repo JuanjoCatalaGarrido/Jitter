442f2a328cf5f3864cefda63bfb3dc466318e2d2
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-12 17:46:38 +0200

    Removed DTOs for compound Ids

--- jitter-core/src/main/java/by/juanjo/jitter/core/dto/ReportDTO.java
+++ jitter-core/src/main/java/by/juanjo/jitter/core/dto/ReportDTO.java
@@ -1,6 +1,5 @@
 package by.juanjo.jitter.core.dto;
 
-import by.juanjo.jitter.core.entity.ReportId;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import java.io.Serializable;
@@ -9,7 +8,7 @@ import lombok.Data;
 
 public @Data class ReportDTO implements Serializable {
 
-  private ReportId id;
+  private Long id;
   private UserSummaryDTO user;
   private PostSummaryDTO post;
   private Integer importance;
@@ -18,7 +17,7 @@ public @Data class ReportDTO implements Serializable {
   private Timestamp updatedAt;
 
   @JsonProperty(access = Access.READ_ONLY)
-  public ReportId getId() {
+  public Long getId() {
     return this.id;
   }
 
