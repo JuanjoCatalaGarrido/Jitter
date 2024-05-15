9c976f35f2bd315eca42a063d0815eccf57685a6
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-15 23:26:17 +0200

    Added derived query to service interfaces

--- jitter-rest/src/main/java/by/juanjo/jitter/rest/service/PostService.java
+++ jitter-rest/src/main/java/by/juanjo/jitter/rest/service/PostService.java
@@ -2,8 +2,9 @@ package by.juanjo.jitter.rest.service;
 
 import by.juanjo.jitter.core.entity.Post;
 import by.juanjo.jitter.rest.service.generic.Service;
+import java.util.List;
 
 public interface PostService extends Service<Post, Long> {
 
-
+  public List<Post> findByOwnerId(Long id);
 }
--- jitter-rest/src/main/java/by/juanjo/jitter/rest/service/UserService.java
+++ jitter-rest/src/main/java/by/juanjo/jitter/rest/service/UserService.java
@@ -2,7 +2,11 @@ package by.juanjo.jitter.rest.service;
 
 import by.juanjo.jitter.core.entity.User;
 import by.juanjo.jitter.rest.service.generic.Service;
+import java.util.List;
 
 public interface UserService extends Service<User, Long> {
 
+  public List<User> findByUsername(String username);
+
+  public List<User> findByEmail(String email);
 }
