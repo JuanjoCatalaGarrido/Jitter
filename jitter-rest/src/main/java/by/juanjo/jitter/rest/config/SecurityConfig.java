7fb3ff73306056670296aa3700ecef96f709bb58
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-26 19:59:41 +0200

    Created configuration for Authenticating

--- jitter-rest/src/main/java/by/juanjo/jitter/rest/config/SecurityConfig.java
+++ jitter-rest/src/main/java/by/juanjo/jitter/rest/config/SecurityConfig.java
@@ -1,36 +1,60 @@
 package by.juanjo.jitter.rest.config;
 
+import by.juanjo.jitter.rest.service.impl.UserDetailsServiceImpl;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
-  @Bean
-  public PasswordEncoder getPasswordEncoder() {
-    return new BCryptPasswordEncoder();
+  private UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+    this.userDetailsService = userDetailsService;
   }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest()
-            .permitAll()).
-        logout(logout -> logout.logoutUrl("/auth/logout")
-            .deleteCookies("JSESSIONID"))
+    http
+        .authorizeHttpRequests(authorize -> authorize
+            .anyRequest().permitAll()
+        )
+        .httpBasic(Customizer.withDefaults())
+        //.addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .formLogin(AbstractHttpConfigurer::disable)
-        .build();
-  }
+        .logout(customizer ->
+            customizer.invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+        );
 
+    return http.build();
+  }
 
   @Bean
   public CorsConfigurationSource corsConfiguration() {
@@ -41,4 +65,21 @@ public class SecurityConfig {
     source.registerCorsConfiguration("/**", corsConfig);
     return source;
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager() {
+    DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+    authenticationProvider.setUserDetailsService(this.userDetailsService);
+    authenticationProvider.setPasswordEncoder(this.passwordEncoder());
+
+    ProviderManager providerManager = new ProviderManager(authenticationProvider);
+    providerManager.setEraseCredentialsAfterAuthentication(true);
+    return providerManager;
+  }
+
 }
