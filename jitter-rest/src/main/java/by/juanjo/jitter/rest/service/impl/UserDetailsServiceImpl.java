3728b8212941afadb4c352e71b34111ddeb7c586
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-26 19:56:58 +0200

    Created classes for setting up Authentication

---
+++ jitter-rest/src/main/java/by/juanjo/jitter/rest/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package by.juanjo.jitter.rest.service.impl;
+
+import by.juanjo.jitter.core.entity.User;
+import by.juanjo.jitter.core.repository.UserRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import by.juanjo.jitter.rest.security.authentication.UserDetailsImpl;
+
+@Service
+public @Data class UserDetailsServiceImpl implements UserDetailsService {
+
+  private UserRepository userRepository;
+
+  @Autowired
+  public UserDetailsServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findOneByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException(
+            String.format("User with username '%s' not found", username)));
+
+    return new UserDetailsImpl(user);
+  }
+
+}
+
