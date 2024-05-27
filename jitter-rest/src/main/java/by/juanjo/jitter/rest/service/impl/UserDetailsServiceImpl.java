c091a8eeb12f861cbd0d7cf67f4f2bced8f7f6df
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-05-27 11:16:24 +0200

    added @Transactional annotation and initialized user.getRoles(). Also added ROLE_ prefix to every role

--- jitter-rest/src/main/java/by/juanjo/jitter/rest/service/impl/UserDetailsServiceImpl.java
+++ jitter-rest/src/main/java/by/juanjo/jitter/rest/service/impl/UserDetailsServiceImpl.java
@@ -3,14 +3,17 @@ package by.juanjo.jitter.rest.service.impl;
 import by.juanjo.jitter.core.entity.User;
 import by.juanjo.jitter.core.repository.UserRepository;
 import lombok.Data;
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import by.juanjo.jitter.rest.security.authentication.UserDetailsImpl;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public @Data class UserDetailsServiceImpl implements UserDetailsService {
 
   private UserRepository userRepository;
@@ -20,11 +23,14 @@ public @Data class UserDetailsServiceImpl implements UserDetailsService {
     this.userRepository = userRepository;
   }
 
+  @Transactional(readOnly = true)
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User user = userRepository.findOneByUsername(username)
         .orElseThrow(() -> new UsernameNotFoundException(
             String.format("User with username '%s' not found", username)));
+
+    Hibernate.initialize(user.getRoles());
 
     return new UserDetailsImpl(user);
   }
