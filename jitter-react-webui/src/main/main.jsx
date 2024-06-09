a7e35ed9186266840a3c38f62899fd31c5e98bbc
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-06-09 15:18:27 +0200

    Configured AuthProvider and fixed layout.scss

--- jitter-react-webui/src/main/main.jsx
+++ jitter-react-webui/src/main/main.jsx
@@ -1,4 +1,6 @@
 'use strict'
+import {AuthProvider} from "./hooks/authentication";
+
 require('file-loader?name=[name].[ext]!./index.html')
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -7,5 +9,7 @@ import App from './App.jsx';
 window.React = React
 
 ReactDOM.createRoot(document.getElementById('react')).render(
-    <App/>
+    <AuthProvider>
+        <App/>
+    </AuthProvider>
 )
