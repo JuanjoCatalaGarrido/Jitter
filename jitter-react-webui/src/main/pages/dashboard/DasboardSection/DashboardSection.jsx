5ae445075e164b1812c68cd8d9b7f463af5c3967
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-06-15 15:46:10 +0200

    Added post creation

--- jitter-react-webui/src/main/pages/dashboard/DasboardSection/DashboardSection.jsx
+++ jitter-react-webui/src/main/pages/dashboard/DasboardSection/DashboardSection.jsx
@@ -5,9 +5,33 @@ import {useLoggedUserInfo, useUserInfo} from "../../../hooks/users";
 import {AuthProvider, useAuth} from "../../../hooks/authentication";
 import {PostsInfiniteScroll} from "./PostsInfiniteScroll/PostsInfiniteScroll";
 import {Icon} from "@iconify/react";
+import axios from "axios";
+import {resolveEndpoint} from "../../../utils/endpoints";
 
 export function DashboardSection({userInfo}) {
-    const {token, userId} = useAuth();
+    const [postText, setPostText] = useState("");
+
+    function handlePostFieldChange(e){
+        setPostText(e.target.value);
+    }
+
+    function handlePostCreation(e){
+        e.preventDefault();
+        let body = {
+            owner : userInfo,
+            body : postText
+        }
+        axios.post(resolveEndpoint("/api/posts"), body)
+            .then((response) => {
+                alert("Post creado correctamente!");
+            })
+            .catch((reason) => {
+            if(!reason.response){
+                alert("No se pudo crear el post");
+            }
+
+        });
+    }
 
   return (
       <section className={"dashboard-section"}>
@@ -17,10 +41,10 @@ export function DashboardSection({userInfo}) {
                   )
               }
           <div className={"newpost-form"}>
-            <textarea></textarea>
+            <textarea value={postText} onChange={handlePostFieldChange}></textarea>
             <div className={"newpost-options"}>
                 <div className={"send-icon-container secondary-bg-color"}>
-                    <Icon icon="material-symbols:send" width={"2rem"}/>
+                    <Icon icon="material-symbols:send" width={"2rem"} onClick={handlePostCreation}/>
                 </div>
                 <div className={"ai-icon-container secondary-bg-color"}>
                     <Icon icon="material-symbols:robot-2-outline" width={"2rem"}/>
