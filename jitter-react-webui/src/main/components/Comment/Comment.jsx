31f8f50e041131213a63c714afecc2dfe58615ad
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-06-16 17:25:52 +0200

    Parsed Timestamp comments

--- jitter-react-webui/src/main/components/Comment/Comment.jsx
+++ jitter-react-webui/src/main/components/Comment/Comment.jsx
@@ -1,6 +1,7 @@
 import './comment.scss';
 
 export function Comment({commentInfo}){
+    const date = new Date(commentInfo.createdAt);
 
     return (
         <div className={"comment"}>
@@ -9,8 +10,15 @@ export function Comment({commentInfo}){
             </div>
             <div className={"comment-content"}>
                 <div className={"comment-content-first-section"}>
-                    <span className={"comment-content-username"}>@{commentInfo.owner.username}</span>
-                    <span className={"comment-content-timestamp"}>{commentInfo.createdAt}</span>
+                    {<a className={"highlight-text-color "} href={`/user/${commentInfo.owner.id}`}>@{commentInfo.owner.username}</a>}
+                    <span className={"comment-content-timestamp"}>{date.toLocaleString('es-ES', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false,
+                    })}</span>
                 </div>
                 <p className={"comment-content-body"}>{commentInfo.body}</p>
             </div>
