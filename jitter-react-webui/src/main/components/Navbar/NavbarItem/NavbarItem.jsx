b26e4fb5b2aad55f757c80371dd63d49b7034636
JuanjoCatalaGarrido <150166697+JuanjoCatalaGarrido@users.noreply.github.com>
2024-06-16 18:26:38 +0200

    Removed frontend

--- jitter-react-webui/src/main/components/Navbar/NavbarItem/NavbarItem.jsx
+++
@@ -1,9 +0,0 @@
-import './navbarItem.scss';
-
-export function NavbarItem({children, href=""}){
-    return (
-        <span className={"navbar-item"}>
-            <a href={href}>{children}</a>
-        </span>
-    );
-}
